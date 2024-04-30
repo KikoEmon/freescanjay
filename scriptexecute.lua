978870a038a0433ee285d707050aa6a42563090d
KikoEmon <166333099+KikoEmon@users.noreply.github.com>
2024-04-30 20:26:42 +0700

    scriptexecute.lua

---
+++ scriptexecute.lua
@@ -0,0 +1,64 @@
+gg.alert("Login to: Public Servers")
+if gg.isPackageInstalled("com.germany.decompile") then
+gg.alert("Please Uninstall Lua Dec App.")
+os.remove(checker)
+print("Please Uninstall Lua Dec App")
+end
+
+if gg.isPackageInstalled("com.hckeam.mjgql") then
+gg.alert("Please Uninstall DEGG App.")
+os.remove(checker)
+print("Please Uninstall DEGG App")
+end
+
+if gg.isPackageInstalled("com.badbunny.luac") then
+gg.alert("Please Uninstall BadBunny Lua Decrypter.")
+os.remove(checker)
+print("Please Uninstall BadBunny Lua Decrypter")
+end
+
+if gg.isPackageInstalled("com.zyt.sstool.premium") then
+gg.alert("Please Uninstall SSTOOL App.")
+os.remove(checker)
+print("Please Uninstall SSTOOL App")
+end
+
+if gg.isPackageInstalled("sstool.only.com.sstool") then
+gg.alert("Please Uninstall SSTOOL App.")
+gg.os.remove(checker)
+print("Please Uninstall SSTOOL App")
+os.exit()
+end
+
+if gg.isPackageInstalled("com.reqable.android") then
+gg.alert("Please Uninstall Reqable App.")
+gg.os.remove(checker)
+print("Please Uninstall Reqable App")
+os.exit()
+end
+
+if gg.isPackageInstalled("com.guoshi.httpcanary") then
+gg.alert("Please Uninstall HttpCannary App.")
+gg.os.remove(checker)
+print("Please Uninstall HttpCannary App")
+os.exit()
+end
+
+if gg.isPackageInstalled("com.sant.httpcanary") then
+gg.alert("Please Uninstall HttpCannary App.")
+gg.os.remove(checker)
+print("Please Uninstall HttpCannary App")
+os.exit()
+end
+
+if gg.isPackageInstalled("xyz.easypro.httpcustom") then
+gg.alert("Please Uninstall Http Custom App.")
+gg.os.remove(checker)
+print("Please Uninstall Http Custom App")
+os.exit()
+end
+
+gg.sleep(100)
+Main()
+-- Hi Api Logger lol
+-- lmao u still need to dec the script not catch the api LOLLL
